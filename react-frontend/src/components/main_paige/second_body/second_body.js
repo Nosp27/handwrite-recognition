32bf4da4e652e655ce7a4ef129dcb98034f06c23
Alena Karnaukhova <st-mks@yandex.ru>
2021-03-26 04:09:09 +0300

    Added photos

--- react-frontend/src/components/main_paige/second_body/second_body.js
+++ react-frontend/src/components/main_paige/second_body/second_body.js
@@ -4,6 +4,7 @@ class SecondBody extends React.Component {
     render() {
         return (
             <section className="second-body-container">
+                <div className="sb-photo-container"></div>
             </section>
         );
     }
--- react-frontend/src/components/main_paige/third_body/third_body.js
+++ react-frontend/src/components/main_paige/third_body/third_body.js
@@ -4,6 +4,10 @@ class ThirdBody extends React.Component {
     render() {
         return (
             <section className="third-body-container">
+                <div className="tb-photo-container Anya"></div>
+                <div className="tb-photo-container Pasha"></div>
+                <div className="tb-photo-container Kolya"></div>
+                <div className="tb-photo-container Alena"></div>
             </section>
         );
     }
